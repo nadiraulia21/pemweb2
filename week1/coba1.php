699109603fa09aa6061dd162b3518e93feb99e2e
Diraaee <115437229+nadiraulia21@users.noreply.github.com>
2023-03-01 23:09:15 +0700

    Add files via upload
    week 1 pw2

---
+++ week1/coba1.php
@@ -0,0 +1,9 @@
+<?php
+
+    define('PHI', 3.14);
+    define('DBNAME', 'Inventori');
+    define('DBSERVER' , 'localhost');
+
+    echo 'Nilai phi: ' . PHI;
+    echo '<br>Nama Databasenya:' . DBNAME;
+    echo '<br> Lokasi Database nya:' . DBSERVER;
---
+++ week1/index.php
@@ -0,0 +1,9 @@
+<?php
+    $nama = 'Nadira Aulia Dewi';
+    $umur = 19;
+    $berat = 60;
+
+    echo 'Nama saya : ' . $nama;
+    echo '<br>Umur saya : ' . $umur;
+    echo '<br>Berat saya : ' . $berat;
+?>
