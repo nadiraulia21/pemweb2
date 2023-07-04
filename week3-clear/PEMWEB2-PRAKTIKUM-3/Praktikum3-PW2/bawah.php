3f1d583936e88b547284faafe8f64b53f3a4b599
Diraaee <115437229+nadiraulia21@users.noreply.github.com>
2023-07-04 18:31:06 +0700

    Add files via upload

---
+++ week3-clear/PEMWEB2-PRAKTIKUM-3/Praktikum3-PW2/bawah.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <footer>
+        <p>Created by
+            <a href="">Lulu Aprilia Wijayanti &copy; 2023</a>
+        </p>  
+    </footer> 
---
+++ week3-clear/PEMWEB2-PRAKTIKUM-3/Praktikum3-PW2/home.php
@@ -0,0 +1,9 @@
+<?php
+    include_once 'atas.php';
+?>
+
+<h1>WELCOME HOME!</H1>
+
+<?php
+    require_once 'bawah.php';
+?>    
